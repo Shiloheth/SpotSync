9aafc158b84c08ad3ba5bb07e0248eb7936bebca
Shiloh <shilohofulue17@gmail.com>
2023-05-03 22:19:35 +0100

    added some divs for the layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 /* eslint-disable react/react-in-jsx-scope */
 import '../styles/index.css'
-
-
+import Page from './search'
+export default function HomePage(){
+  
+return(
+  <Page/>
+)
+}
 
   
